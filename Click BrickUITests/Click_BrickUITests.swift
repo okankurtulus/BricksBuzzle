d7ae05ce8b6ef55e169826c864202e0bf2ed0e51
Okan Kurtulus <okankurtulus@gmail.com>
2016-08-20 00:05:11 +0300

    - Added fastlane

--- Click BrickUITests/Click_BrickUITests.swift
+++ Click BrickUITests/Click_BrickUITests.swift
@@ -14,12 +14,14 @@ class Click_BrickUITests: XCTestCase {
         super.setUp()
         
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        let app = XCUIApplication()
+        setupSnapshot(app)
         
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
-
+        app.launch()
+        
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
     
@@ -28,9 +30,28 @@ class Click_BrickUITests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+     func testTake5SS1() { takeAScreenShot(1) }
+     func testTake5SS2() { takeAScreenShot(2) }
+     func testTake5SS3() { takeAScreenShot(3) }
+     func testTake5SS4() { takeAScreenShot(4) }
+     func testTake5SS5() { takeAScreenShot(5) }
+    
+    
+    func takeAScreenShot(index : Int) {
+        
+        var element = XCUIApplication().childrenMatchingType(.Window).elementBoundByIndex(0).childrenMatchingType(.Other).element.childrenMatchingType(.Other).elementBoundByIndex(1)
+        element.tap()
+        element = XCUIApplication().childrenMatchingType(.Window).elementBoundByIndex(0).childrenMatchingType(.Other).element.childrenMatchingType(.Other).elementBoundByIndex(1)
+        element.tap()
+        element = XCUIApplication().childrenMatchingType(.Window).elementBoundByIndex(0).childrenMatchingType(.Other).element.childrenMatchingType(.Other).elementBoundByIndex(1)
+        element.tap()
+        element = XCUIApplication().childrenMatchingType(.Window).elementBoundByIndex(0).childrenMatchingType(.Other).element.childrenMatchingType(.Other).elementBoundByIndex(1)
+        element.tap()
+        element = XCUIApplication().childrenMatchingType(.Window).elementBoundByIndex(0).childrenMatchingType(.Other).element.childrenMatchingType(.Other).elementBoundByIndex(1)
+        element.tap()
+        
+        snapshot("ScreenShot_\(index)")
     }
     
 }
